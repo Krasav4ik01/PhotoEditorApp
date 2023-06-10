ecf8beee98cd30f08bce524cb59dd2d335e24805
rustem <200103424@stu.sdu.edu.kz>
2023-06-10 23:20:50 +0600

    добавил DsPhotoEditor

--- app/src/main/java/com/example/photoeditorapp/MainActivity.kt
+++ app/src/main/java/com/example/photoeditorapp/MainActivity.kt
@@ -1,13 +1,51 @@
 package com.example.photoeditorapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ImageButton
+import com.dsphotoeditor.sdk.activity.DsPhotoEditorActivity
+import com.dsphotoeditor.sdk.utils.DsPhotoEditorConstants
 
 class MainActivity : AppCompatActivity() {
+    lateinit var imageButtonUpload: ImageButton
+    val IMAGE_REQUEST_CODE: Int = 101
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageButtonUpload = findViewById(R.id.btn_upload)
+        imageButtonUpload.setOnClickListener {
+            val intent = Intent()
+            intent.setAction(Intent.ACTION_GET_CONTENT)
+            intent.setType("image/*")
+            startActivityForResult(intent, IMAGE_REQUEST_CODE)
+//            startActivity(intent)
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == IMAGE_REQUEST_CODE){
+            if (data?.data != null){
+                var filePath: Uri? = data.data
+                val dsPhotoEditorIntent = Intent(this, DsPhotoEditorActivity::class.java)
+                dsPhotoEditorIntent.data = filePath
+                dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY, "YOUR_OUTPUT_IMAGE_FOLDER")
+
+                val toolsToHide = intArrayOf(DsPhotoEditorActivity.TOOL_ORIENTATION, DsPhotoEditorActivity.TOOL_CROP)
+                dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE, toolsToHide)
+
+                startActivityForResult(dsPhotoEditorIntent, 200)
+
+            }
+        }
+        if (requestCode == 200){
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.setData(data?.data)
+            startActivity(intent)
+        }
+    }
+
 }
--- app/src/main/java/com/example/photoeditorapp/ResultActivity.kt
+++ app/src/main/java/com/example/photoeditorapp/ResultActivity.kt
@@ -2,10 +2,14 @@ package com.example.photoeditorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class ResultActivity : AppCompatActivity() {
+    lateinit var prevImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+        prevImage = findViewById(R.id.image_result)
+        prevImage.setImageURI(intent.data)
     }
 }
